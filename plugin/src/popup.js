4a5ad1943499c2a8f82d3fb68f538930dc1d05b1
TongyunHuang <tongyun@simplyicard.com>
2022-05-09 23:42:30 -0500

    multiple label

--- plugin/src/popup.js
+++ plugin/src/popup.js
@@ -5,13 +5,23 @@ let detectPost = document.getElementById("detectPost");
 
 // When the button is clicked, inject addFakePostLabel into current page
 detectPost.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['script.js']
-  });
+    //let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+            console.log(response.farewell)
+            alert("Start Detecting!");
+        });
+    });
 });
+
+// Initialize close button
+let closeBtn = document.getElementById('x');
+
+// When the close button is clicked, close the popup window
+closeBtn.addEventListener("click", async () => {
+    window.close();
+})
 
 
 
